9007b6e560b473790c72a00da34d70fb2cde26aa
Yesid Romero <augustomero@gmail.com>
2026-02-17 22:24:21 -0500

    "Fix: Database initialization and Mocking for Unit Tests"

---
+++ __tests__/categories.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const express = require('express');
+const mongodb = require('../db/connect');
+const router = require('../routes/categories');
+
+jest.mock('../middleware/auth', () => ({
+    isAuthenticated: (req, res, next) => next()
+}));
+
+const app = express();
+app.use(express.json());
+app.use('/categories', router);
+
+beforeAll(async () => {
+    return new Promise((resolve, reject) => {
+        mongodb.initDb((err) => {
+            if (err) reject(err);
+            else resolve();
+        });
+    });
+});
+
+test('GET /categories should return 200', async () => {
+    const res = await request(app).get('/categories');
+    console.log('API Response:', res.body);
+    expect(res.statusCode).toBe(200);
+});
